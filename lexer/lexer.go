5f27e2f1c4246df2bc14c70881622d4c9946ca21
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-07-26 21:52:48 +0530

    adding support more keywords and symbol is != and ==

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,8 +2,6 @@ package lexer
 
 import "laait/token"
 
-// import "laait/token/token"
-
 type Lexer struct {
 	input        string
 	position     int
@@ -33,7 +31,33 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -42,8 +66,6 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.RTPAREN, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
 	case '{':
 		tok = newToken(token.LTBRACE, l.ch)
 	case '}':
@@ -109,5 +131,13 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
 	}
 }
--- lexer/lexer_test/lexer_test.go
+++ lexer/lexer_test/lexer_test.go
@@ -15,6 +15,10 @@ func TestNextToken(t *testing.T) {
         x + y;
     };
     mini result = add(five, ten);
+    !-/*5;
+    5 < 10 > 5;
+	10 == 10
+	10 != 9
     `
 
 	// var Token struct {
@@ -77,6 +81,24 @@ func TestNextToken(t *testing.T) {
 		{token.NOUN, "ten"},
 		{token.RTPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
 		{token.FILEEND, ""},
 	}
 
