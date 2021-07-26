321e2385d5657d5d944916b6b97c15380a10ac6f
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-07-26 22:20:23 +0530

    addin repl

--- laait.go
+++ laait.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"laait/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s This is LAAIT(lite) Progrmming Language.\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+	fmt.Println("\nThanks to use LAAIT")
 }
