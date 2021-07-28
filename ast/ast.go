38dcaf56931729845d69e1186702667c8b27f508
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-07-28 20:36:37 +0530

    adding i in parameter

--- ast/ast.go
+++ ast/ast.go
@@ -1,27 +1,45 @@
 package ast
 
-type Node interface{
-    TokenLiteral() string
+import "go/token"
+
+type Node interface {
+	TokenLiteral() string
 }
 
-type Statement interface{
-    Node
-    statementNode()
+type Statement interface {
+	Node
+	statementNode()
 }
 
-type Expression interface{
-    Node 
-    expressionNode()
+type Identifier struct {
+	Token token.Token
+	Value string
 }
 
-type Program struct{
-    Statements []Statement
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
 }
 
-func (p *Program) TokenLiteral() string{
-    if len(p.statements) > 0 {
-        return p.Statements[0].TokenLiteral()
-    } else {
-        return ""
-    }
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (ls *LetStatement) statementNode()
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (*Identifier) expressionNode()        {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
 }
