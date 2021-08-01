eb76e0fa008f6e10a47106af44e4b2fe6394e401
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-08-01 22:30:11 +0530

    adding some code for parseStatement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -27,11 +27,11 @@ func TestLetStatements(t *testing.T){
         expectedIndentifier string
     }{
         {"x"},
-        {"y"}
-        {"foobar"}
+        {"y"},
+        {"foobar"},
     }
 
-    for i, tt : range tests{
+    for i, tt := range tests{
         stmt := program.Statements[i]
         if !testLetStatement(t, stmt, tt.expectedIdentifier){
             return
@@ -41,9 +41,23 @@ func TestLetStatements(t *testing.T){
 
 func testLetStatement( t *testing.T, s ast.Statement, name string ) bool{
     if s.TokenLiteral() != "let" || s.TokenLiteral() != "bind" {
-        t.Error("s.TokenLiteral not 'let'. got = %q", s.TokenLiteral())
+        t.Error("s.TokenLiteral not 'let' or 'bind'. got = %q", s.TokenLiteral())
         return false
     }
 
-    letStmt, o
+    letStmt, ok := s.(*ast.LetStatement)
+    if !ok {
+        t.Errorf("s not *ast.LetStatement. got=%T", s)
+        return false
+    }
+
+    if letStmt.Name.Value != name{
+        t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+        return false
+    }
+    if letStmt.Name.TokenLiteral() != name{
+        t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+        return false
+    }
+    return true
 }
