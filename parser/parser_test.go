1aca804951c96ef03ce489582a47b5127f3f77a7
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-08-03 21:24:05 +0530

    adding test case for bind or let

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,63 +1,66 @@
-package parser
+package parser_test
 
 import (
-    "testing"
-    "laait/ast"
-    "laait/lexer"
+	"fmt"
+	"laait/ast"
+	"laait/lexer"
+	"laait/parser"
+	"testing"
 )
 
-func TestLetStatements(t *testing.T){
-    input := `
+func TestLetStatements(t *testing.T) {
+	input := `
     let x = 5;
-    let y = 10;
-    let foobar = 8383838;
+    bind  y = 10;
+    let  foobar = 1221221;
     `
-    l := lexer.New(input)
-    p := New()
+	l := lexer.New(input)
+	p := parser.New(l)
 
-    program := p.ParseProgram()
-    if program == nil{
-        t.Fatalf("Parse Program( returned nil)")
-    }
-    if len(program.Statements) != 3{
-        t.Fatalf("program.Statements does not contain 3 statement.got= %d", len(program.Statements))
-    }
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("Parse Program( returned nil)")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statement.got= %d", len(program.Statements))
+	}
 
-    test:= []struct{
-        expectedIndentifier string
-    }{
-        {"x"},
-        {"y"},
-        {"foobar"},
-    }
+	tests := []struct {
+		expectedIndentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
 
-    for i, tt := range tests{
-        stmt := program.Statements[i]
-        if !testLetStatement(t, stmt, tt.expectedIdentifier){
-            return
-        }
-    }
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIndentifier) {
+			return
+		}
+	}
 }
 
-func testLetStatement( t *testing.T, s ast.Statement, name string ) bool{
-    if s.TokenLiteral() != "let" || s.TokenLiteral() != "bind" {
-        t.Error("s.TokenLiteral not 'let' or 'bind'. got = %q", s.TokenLiteral())
-        return false
-    }
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	fmt.Println(s.TokenLiteral() == "let")
+	if s.TokenLiteral() != "let" && s.TokenLiteral() != "bind" {
+		t.Errorf("s.TokenLiteral not 'let' or 'bind'. got = %q", s.TokenLiteral())
+		return false
+	}
 
-    letStmt, ok := s.(*ast.LetStatement)
-    if !ok {
-        t.Errorf("s not *ast.LetStatement. got=%T", s)
-        return false
-    }
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
 
-    if letStmt.Name.Value != name{
-        t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
-        return false
-    }
-    if letStmt.Name.TokenLiteral() != name{
-        t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
-        return false
-    }
-    return true
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
 }
