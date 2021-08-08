721b172e42d3d7c938ad44df83c08ec9bd046f35
Rahul Beniwal <rahul.b@lendenclub.com>
2021-08-08 22:14:56 +0530

    adding test case for return statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,7 +11,7 @@ import (
 func TestLetStatements(t *testing.T) {
 	input := `
     let x = 5;
-    let  = 10;
+    let y = 10;
     let  foobar = 1221221;
     `
 	l := lexer.New(input)
@@ -38,6 +38,36 @@ func TestLetStatements(t *testing.T) {
 		stmt := program.Statements[i]
 		if !testLetStatement(t, stmt, tt.expectedIndentifier) {
 			return
+		}
+	}
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+    return 5;
+    return 10;
+    return 993322;
+    `
+
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got = %d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
 		}
 	}
 }
