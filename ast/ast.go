7b0246bb3d993204500a375145831d98117655ef
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-09-23 14:25:18 +0530

    adding debugging String method for Expression Types

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "laait/token"
+import (
+	"bytes"
+	"laait/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string // soley use for debugging purposes
 }
 
 type Statement interface {
@@ -22,9 +26,9 @@ type LetStatement struct {
 	Value Expression
 }
 
-type ReturnStatement struct{
-    Token token.Token
-    ReturnValue Expression
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
 }
 
 type Expression interface {
@@ -36,13 +40,70 @@ type Program struct {
 	Statements []Statement
 }
 
+func (i *Identifier) String() string { return i.Value }
+
+// implementing String for Program
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// dubugging for Let Statement
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString("=")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
+// complex statement expression
+type ExpressionStatement struct {
+	Token      token.Token // each instruction has atleast one expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-func (rs *ReturnStatement) statementNode()    {}
+func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
-func (*Identifier) expressionNode()        {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (*Identifier) expressionNode()              {}
+func (i *Identifier) TokenLiteral() string       { return i.Token.Literal }
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
