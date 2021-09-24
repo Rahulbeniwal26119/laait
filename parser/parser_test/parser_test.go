fc51b112702853c72a1b6abdaf28d46eb0c30935
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-09-24 13:53:22 +0530

    adding test case for expression and integer types

--- parser/parser_test/parser_test.go
+++ parser/parser_test/parser_test.go
@@ -13,7 +13,6 @@ func TestLetStatements(t *testing.T) {
     let x = 5;
     let y = 10;
     let  foobar = 1221221;
-	let 99999;
     `
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -137,5 +136,35 @@ func TestIndentifierExpression(t *testing.T) {
 
 	if indent.TokenLiteral() != "foobar" {
 		t.Errorf("indent.TokenLiteral not %s. got %s", "foobar", indent.TokenLiteral())
+	}
+}
+
+func TestIntegerLiteral(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 1 {
+		t.Fatalf("Program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not a ast.ExpressionStatement. got %T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Errorf("literal.Value not %d. got = %d", 5, literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral not %s. got %s", "5", literal.TokenLiteral())
 	}
 }
