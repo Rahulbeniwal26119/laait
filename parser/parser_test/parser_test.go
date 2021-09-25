c44c0c46908da158f5eab9e6626f2fb5e922d130
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-09-25 14:20:35 +0530

    adding test for prefix expressions

--- parser/parser_test/parser_test.go
+++ parser/parser_test/parser_test.go
@@ -168,3 +168,97 @@ func TestIntegerLiteral(t *testing.T) {
 		t.Errorf("literal.TokenLiteral not %s. got %s", "5", literal.TokenLiteral())
 	}
 }
+
+func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
+	integer, ok := il.(*ast.IntegerLiteral)
+	if !ok {
+		t.Errorf("il not *ast.IntegerLiteral. got=%T", il)
+		return false
+	}
+
+	if integer.Value != value {
+		t.Errorf("integer.Value not %d. got=%d", value, integer.Value)
+		return false
+	}
+
+	if integer.TokenLiteral() != fmt.Sprintf("%d", value) {
+		t.Errorf("integer.TokenLiteral not %d. got=%s", value, integer.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
+func TestParsingPrefixExpression(t *testing.T) {
+	prefixTests := []struct {
+		input        string
+		operator     string
+		integerValue int64
+	}{
+		{"!5;", "!", 5},
+		{"-15", "-", 15},
+	}
+
+	for _, tt := range prefixTests {
+		l := lexer.New(tt.input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+				1, len(program.Statements))
+		}
+
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statement[0] is not ast.ExpressionStatement, got=%T", program.Statements[0])
+		}
+
+		expr, ok := stmt.Expression.(*ast.PrefixExpression)
+		if !ok {
+			t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
+		}
+
+		if expr.Operator != tt.operator {
+			t.Fatalf("exp.Operator is not '%s'. got=%s", tt.operator, expr.Operator)
+		}
+
+		if expr.Operator != tt.operator {
+			if !testIntegerLiteral(t, expr.Right, tt.integerValue) {
+				return
+			}
+		}
+	}
+}
+
+func TestParsingInfixExpressions(t *testing.T){
+	infixTest := []struct{
+		input string 
+		leftValue int64
+		operator string 
+		rightValue int64
+	}
+	{
+		{"5 + 5", 5 , "+", 5},
+		{"5 - 5", 5 , "-", 5},
+		{"5 * 5", 5 , "*", 5},
+		{"5 / 5", 5 , "/", 5},
+		{"5 > 5", 5 , ">", 5},
+		{"5 < 5", 5 , "<", 5},
+		{"5 == 5", 5 , "==", 5},
+		{"5 != 5", 5 , "!=", 5},
+	}
+
+	for _, tt := range infixTest {
+		l := lexer.New(tt.input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != -1{
+			t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+		)
+		}
+	}
+}
