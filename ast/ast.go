8c249a80b61d92217dfaef57a7185ffecb6e67be
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-09-27 22:55:04 +0530

    adding type for boolean literals

--- ast/ast.go
+++ ast/ast.go
@@ -58,6 +58,15 @@ type InfixExpression struct {
 	Right    Expression
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
+
 func (pe *PrefixExpression) expressionNode()      {}
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
