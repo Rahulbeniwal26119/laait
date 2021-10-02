8f294a4ac8b651e449fabb982a8b8243bc9cb0db
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-02 09:37:23 +0530

    adding type object for boolean and null

--- object/object.go
+++ object/object.go
@@ -8,6 +8,8 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -19,6 +21,23 @@ type Integer struct {
 	Value int64
 }
 
+type Boolean struct {
+	Value bool
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+func (n *Null) Inspect() string { return "null" }
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
 func (i *Integer) Inspect() string {
 	return fmt.Sprintf("%d", i.Value)
 }
