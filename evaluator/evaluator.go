ad05ebe3d7ef52677c2496a40fc7e19d2d40779e
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-06 10:34:20 +0530

    adding evaluator for if

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -29,6 +29,12 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	// Expression
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -37,6 +43,32 @@ func Eval(node ast.Node) object.Object {
 		return nativeBoolToBoolObject(node.Value)
 	}
 	return nil
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Effect)
+	} else if ie.Optional != nil {
+		return Eval(ie.Optional)
+	} else {
+		return NULL
+	}
+
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 func evalInfixExpression(
