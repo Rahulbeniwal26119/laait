7ffad674eb4c95dda4d4197c9d0f78cded698bbb
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-08 13:53:16 +0530

    adding return type

--- object/object.go
+++ object/object.go
@@ -7,10 +7,15 @@ import (
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
+
+type ReturnValue struct {
+	Value Object
+}
 
 type Object interface {
 	Type() ObjectType
@@ -43,3 +48,11 @@ func (i *Integer) Inspect() string {
 }
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
+}
+
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
+}
