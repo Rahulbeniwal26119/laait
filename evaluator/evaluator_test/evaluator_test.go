62a58b89c064dcd03a8cd1aa9d7875ed9386a404
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-13 09:28:48 +0530

    adding test for more complex function evaluation

--- evaluator/evaluator_test/evaluator_test.go
+++ evaluator/evaluator_test/evaluator_test.go
@@ -248,3 +248,44 @@ func TestLetStatement(t *testing.T) {
 		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
+
+func TestFunctionObject(t *testing.T) {
+	input := "function(x) { x + 2; };"
+	evaluated := testEval(input)
+
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not function. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters. Parameters=%+v", fn.Parameters)
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = function(x) { x;} identity(5);", 5},
+		{"let identity = function(x) { return x;} identity(5);", 5},
+		{"let double = function(x) { x * 2;}; double(5);", 10},
+		{"let add = function(x,y) { x + y;}; add(5,5);", 10},
+		{"let add = function(x,y) { x + y;}; add(5+5, add(5,5));", 20},
+		{"function(x);(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
