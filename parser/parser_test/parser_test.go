14ee97ae463f613ecd03f84c2a8c8f0dc770afd9
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-22 22:43:52 +0530

    adding test case for parsing array and array index

--- parser/parser_test/parser_test.go
+++ parser/parser_test/parser_test.go
@@ -418,6 +418,10 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"add(a + b + c * d / f + g)",
 			"add((((a + b) + ((c * d) / f)) + g))",
 		},
+		{
+			"a * [1, 2, 3, 4][b * c] * d",
+			"((a * ([1, 2, 3, 4][(b * c)])) * d)",
+		},
 	}
 
 	for _, tt := range tests {
