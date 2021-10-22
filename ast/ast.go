0c24b312364520137a745a392f6034cc391a78a8
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-22 22:41:01 +0530

    fixing string representation of arrays

--- ast/ast.go
+++ ast/ast.go
@@ -128,7 +128,7 @@ func (al *ArrayLiteral) String() string {
 	}
 
 	out.WriteString("[")
-	out.WriteString(strings.Join(elements, ","))
+	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
 
 	return out.String()
