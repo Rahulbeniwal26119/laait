a3f42aa81fc081248bac2811c12ba3fff685ae5b
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-10-27 21:43:03 +0530

    adding ast for hash table

--- ast/ast.go
+++ ast/ast.go
@@ -289,6 +289,27 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+type HashLiteral struct {
+	Token token.Token
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ","))
+	out.WriteString("}")
+
+	return out.String()
+}
+
 func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 
