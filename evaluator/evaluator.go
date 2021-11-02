f07bcb9dd000fc94de87bcd5f630a836a80261c5
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-11-02 20:48:53 +0530

    adding evaluator for quote and unquote

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -82,7 +82,7 @@ func Eval(node ast.Node, env *environment.Environment) object.Object {
 		return &object.String{Value: node.Value}
 	case *ast.CallExpression:
 		if node.Function.TokenLiteral() == "quote" {
-			return quote(node.Arguments[0])
+			return quote(node.Arguments[0], env)
 		}
 		function := Eval(node.Function, env)
 		if isError(function) {
