270d754401b6ae706aa13284530c6b1c936fd840
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-11-06 14:14:36 +0530

    adding test case for expand macros

--- evaluator/evaluator_test/macro_evaluator_test.go
+++ evaluator/evaluator_test/macro_evaluator_test.go
@@ -63,3 +63,47 @@ func testParseProgram(input string) *ast.Program {
 	p := parser.New(l)
 	return p.ParseProgram()
 }
+
+func TestExpandMacros(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`let infixExpression = macro() { quote(1 + 2); };
+			infixExpression();`,
+			`(1 + 2)`,
+		},
+		{
+			`
+			let reverse = macro(a,b) { quote(unquote(b) - unquote(a)); };
+			reverse(2 + 2, 10 - 5);
+			`,
+			`(10 - 5) - (2 + 2)`,
+		},
+		{
+			`let unless = macro(condition, consequence, alternative){
+				quote(if ( !(unquote(condition))){
+					unquote(consequence);
+				} else {
+					unquote(alternative);
+				});
+			};
+			unless(10 > 5, puts("not greater"), puts("greater"));`,
+			`if (!(10 > 5)) { puts("not greater") } else { puts("greater") }`,
+		},
+	}
+	for _, tt := range tests {
+		expected := testParseProgram(tt.expected)
+		program := testParseProgram(tt.input)
+
+		env := environment.NewEnvironment()
+		evaluator.DefineMacros(program, env)
+		expanded := evaluator.ExpandMacros(program, env)
+
+		if expanded.String() != expected.String() {
+			t.Errorf("not equal. want=%q, got=%q",
+				expected.String(), expanded.String())
+		}
+	}
+}
