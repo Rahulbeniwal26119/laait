fff529cf681090404d2fc4caa2c5412be0f4b499
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-11-23 07:21:28 +0530

    adding test for string representation of instruction

--- code/code_testing/code_test.go
+++ code/code_testing/code_test.go
@@ -30,3 +30,54 @@ func TestMake(t *testing.T) {
 		}
 	}
 }
+
+func TestInstructionString(t *testing.T) {
+	instructions := []code.Instructions{
+		code.Make(code.OpConstant, 1),
+		code.Make(code.OpConstant, 2),
+		code.Make(code.OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatted := code.Instructions{}
+	for _, ins := range instructions {
+		concatted = append(concatted, ins...)
+	}
+
+	if concatted.String() != expected {
+		t.Errorf("instructions wrongly formatted.\nwant=%q\ngot=%q",
+			expected, concatted.String())
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        code.Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{code.OpConstant, []int{65535}, 2},
+	}
+
+	for _, tt := range tests {
+		instruction := code.Make(tt.op, tt.operands...)
+		def, err := code.Lookup(byte(tt.op))
+		if err != nil {
+			t.Fatalf("definition not found: %q\n", err)
+		}
+
+		operandsRead, n := code.ReadOperands(def, instruction[1:])
+		if n != tt.bytesRead {
+			t.Fatalf("n wrong. want=%d, got=%d", tt.bytesRead, n)
+		}
+		for i, want := range tt.operands {
+			if operandsRead[i] != want {
+				t.Errorf("operand wrong. want=%d, got=%d", want, operandsRead[i])
+			}
+		}
+	}
+}
