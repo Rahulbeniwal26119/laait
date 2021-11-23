570160e81420ccb2b74610da8fceafa296e30779
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-11-23 07:20:52 +0530

    adding string representation for instructions

--- code/code.go
+++ code/code.go
@@ -1,6 +1,7 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
@@ -55,4 +56,56 @@ func Lookup(op byte) (*Definition, error) {
 	}
 
 	return def, nil
+}
+
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return operands, offset
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
+}
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, ins[i+1:])
+
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstructions(def, operands))
+		i += 1 + read
+	}
+	return out.String()
+}
+
+func (ins Instructions) fmtInstructions(def *Definition, operands []int) string {
+	operandsCount := len(def.OperandWidths)
+
+	if len(operands) != operandsCount {
+		return fmt.Sprintf("ERROR: operands len %d does not match defined %d\n", len(operands), operandsCount)
+	}
+
+	switch operandsCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
 }
