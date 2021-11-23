cc3d5f95d2c46e2e43cedbe124ed33f3a0f0f4dc
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-11-23 07:21:47 +0530

    start writing main compilation part for instructions

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -24,6 +24,34 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		// Todo
+	}
 	return nil
 }
 
