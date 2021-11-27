485e33ddab9d28e0c9104d2e98187261674a6a2a
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-11-27 20:51:53 +0530

    fixing compiler repl for pop opcode

--- compiler_repl/compiler_repl.go
+++ compiler_repl/compiler_repl.go
@@ -54,7 +54,7 @@ func Start(in io.Reader, out io.Writer, show_bytecode bool) {
 			continue
 		}
 		//
-		stackTop := machine.StackTop()
+		stackTop := machine.LastPoppedStackElem()
 		io.WriteString(out, stackTop.Inspect())
 		io.WriteString(out, "\n")
 		count++
