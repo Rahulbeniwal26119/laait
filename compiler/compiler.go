9f5a114fe0ebff92b6334744785184a0539aab07
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2022-01-16 16:24:48 +0530

    adding support for global variable and adding symbol table in compiler too

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -16,6 +16,7 @@ type Compiler struct {
 	constants           []object.Object
 	lastInstruction     EmittedInstruction
 	previousInstruction EmittedInstruction
+	symbolTable         *SymbolTable
 }
 
 type Bytecode struct {
@@ -29,6 +30,7 @@ func New() *Compiler {
 		constants:           []object.Object{},
 		lastInstruction:     EmittedInstruction{},
 		previousInstruction: EmittedInstruction{},
+		symbolTable:         NewSymbolTable(),
 	}
 }
 
@@ -163,11 +165,21 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OPFALSE)
 		}
 
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+		if !ok {
+			return fmt.Errorf("undefined variable %s", node.Value)
+		}
+
+		c.emit(code.OPGETGLOBAL, symbol.Index)
+
 	case *ast.LetStatement:
 		err := c.Compile(node.Value)
 		if err != nil {
 			return err
 		}
+		symbol := c.symbolTable.Define(node.Name.Value)
+		c.emit(code.OPSETGLOBAL, symbol.Index)
 	}
 
 	return nil
--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -21,3 +21,15 @@ func NewSymbolTable() *SymbolTable {
 	s := make(map[string]Symbol)
 	return &SymbolTable{store: s}
 }
+
+func (s *SymbolTable) Define(name string) Symbol {
+	symbol := Symbol{Name: name, Index: s.numDefinations, Scope: GlobalScope}
+	s.store[name] = symbol
+	s.numDefinations++
+	return symbol
+}
+
+func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
+	obj, ok := s.store[name]
+	return obj, ok
+}
