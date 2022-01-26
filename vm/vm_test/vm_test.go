b1863df438d17d33a87d8e6535362a4f443eaf48
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2022-01-26 19:59:52 +0530

    adding support for vm

--- vm/vm_test/vm_test.go
+++ vm/vm_test/vm_test.go
@@ -64,6 +64,25 @@ func testExpectedObject(
 		if err != nil {
 			t.Errorf("testStringObject failed: %s", err)
 		}
+	case []int:
+		array, ok := actual.(*object.Array)
+		if !ok {
+			t.Errorf("object not Array : %T (%+v)", actual, actual)
+			return
+		}
+
+		if len(array.Elements) != len(expected) {
+			t.Errorf("wrong num of elements. want=%d, got=%d",
+				len(expected), len(array.Elements))
+			return
+		}
+
+		for i, expectedElem := range expected {
+			err := testIntegerObject(int64(expectedElem), array.Elements[i])
+			if err != nil {
+				t.Errorf("testIntegerObject failed: %s", err)
+			}
+		}
 	}
 }
 
@@ -202,6 +221,16 @@ func TestStringExpressions(t *testing.T) {
 		{`"laait"`, "laait"},
 		{`"laa" + "key"`, "laakey"},
 		{`"sim" + "ple" + "laait"`, "simplelaait"},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestArrayLiteral(t *testing.T) {
+	tests := []vmTestCase{
+		{"[]", []int{}},
+		{"[1,2,3]", []int{1, 2, 3}},
+		{"[1+2, 3 * 4, 5 + 6]", []int{3, 12, 11}},
 	}
 
 	runVmTests(t, tests)
