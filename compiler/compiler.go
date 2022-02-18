3683e00d1b8ab4358703c1dc75222b4a418cc0b2
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2022-02-18 22:39:44 +0530

    adding scopes for compiler

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -269,7 +269,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
 
-		c.emit(code.OPGETGLOBAL, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OPGETGLOBAL, symbol.Index)
+		} else {
+			c.emit(code.OPGETLOCAL, symbol.Index)
+		}
 
 	case *ast.LetStatement:
 		err := c.Compile(node.Value)
@@ -277,7 +281,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 		symbol := c.symbolTable.Define(node.Name.Value)
-		c.emit(code.OPSETGLOBAL, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OPSETGLOBAL, symbol.Index)
+		} else {
+			c.emit(code.OPSETLOCAL, symbol.Index)
+		}
 	}
 
 	return nil
@@ -371,12 +379,14 @@ func (c *Compiler) enterScope() {
 
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
 	instructions := c.currentInstructions()
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+	c.symbolTable = c.symbolTable.Outer
 	return instructions
 }
 
