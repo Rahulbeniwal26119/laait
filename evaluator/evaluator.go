65077b6f432afed66ecd2f576bb9d44a0450d40f
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2022-02-28 22:26:47 +0530

    moving all builtins to object

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -168,7 +168,10 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 		return unwrapReturnValue(evaluated)
 
 	case *object.Builtin:
-		return fn.Fn(args...)
+		if result := fn.Fn(args...); result != nil {
+			return result
+		}
+		return NULL
 
 	default:
 		return newError("not a function: %s", fn.Type())
@@ -425,81 +428,12 @@ func evalStringInfixExpression(
 }
 
 var builtins = map[string]*object.Builtin{
-	"len": object.GetBuiltinsByName("len"),
-	"first": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got = %d, want=1", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			if len(arr.Elements) > 0 {
-				return arr.Elements[0]
-			}
-
-			return NULL
-		},
-	},
-	"last": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
-			}
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-			if length > 0 {
-				return arr.Elements[length-1]
-			}
-
-			return NULL
-		},
-	},
-	"rest": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got = %d, want=1", len(args))
-			}
-
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `rest` must be Array, got '%s'", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-			if length > 0 {
-				newElements := make([]object.Object, length-1, length-1)
-				copy(newElements, arr.Elements[1:length])
-				return &object.Array{Elements: newElements}
-			}
-			return NULL
-		},
-	},
-	"push": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to push must be ARRAY, got %s", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-
-			newElements := make([]object.Object, length+1, length+1)
-			copy(newElements, arr.Elements)
-			newElements[length] = args[1]
-
-			return &object.Array{Elements: newElements}
-		},
-	},
-	"puts": object.GetBuiltinsByName("puts"),
+	"len":   object.GetBuiltinsByName("len"),
+	"first": object.GetBuiltinsByName("first"),
+	"last":  object.GetBuiltinsByName("last"),
+	"rest":  object.GetBuiltinsByName("rest"),
+	"push":  object.GetBuiltinsByName("push"),
+	"puts":  object.GetBuiltinsByName("puts"),
 }
 
 func evalHashLiteral(
