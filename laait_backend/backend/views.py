52d6c001205745560c1812a8a5e279e64dbf57f3
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2022-03-30 07:45:49 +0530

    adding error message supprt

--- laait_backend/backend/views.py
+++ laait_backend/backend/views.py
@@ -1,8 +1,9 @@
 import os
+from urllib import response
 from backend.models import Notebook as NB 
 from rest_framework.decorators import api_view
 from backend.forms import NoteBookForm
-from shared.shared_utils import log_and_respond
+from shared.shared_utils import log_and_respond, check_error_message
 from rest_framework import status 
 from os import system 
 import simplejson as json 
@@ -22,11 +23,9 @@ def notebook_view(request):
                 status=  status.HTTP_400_BAD_REQUEST
             )
         input_code = request.POST.get("input")
-        # input_code = input_form_data.get("input")
         
 
         with open('read_text.txt', 'w') as f:
-            # print(input_code, file=f)
             f.write(input_code)
         
 
@@ -46,15 +45,27 @@ def notebook_view(request):
         system('laait_nb_interpreter evaluator notebook')
 
         # if command is successful it create a output file in 
+        response_code = ''
         output = []
         with open('output_.txt', 'r') as f:
             output = f.readlines()
         if not output:
             output = 'No output. It is under developement'
+            response_code = 'DEVEL'
 
         output = ''.join(output)
+        if output:
+            if check_error_message(output):
+                response_code = 'ERROR'
+            else:
+                response_code = 'SUCCESS'
+
         NB.objects.create(input=input_code, output=output)
-        return log_and_respond(data = output, 
+        data = {
+            "output" : output,
+            "response_code": response_code
+        } 
+        return log_and_respond(data = data, 
         status = status.HTTP_200_OK,message="Interpreted Successfully")
     except Exception as e:
         return log_and_respond(
@@ -62,44 +73,3 @@ def notebook_view(request):
             message = str(e),
             status=  status.HTTP_500_INTERNAL_SERVER_ERROR,
             exception=e)
-
-
-
-
-
-# def notebook_view(request):
-    # try:
-        # input_form_data = json.loads(request.body.decode("utf-8"))
-        # print(input_form_data)
-        # if not input_form_data.get("input"):
-            # return log_and_respond(
-                # data = None,
-                # message = "Input cannot be null",
-                # status=  status.HTTP_400_BAD_REQUEST
-            # )
-        # input_code = input_form_data.get("input")
-        # 
-# 
-        # with open('read_text.txt', 'w') as f:
-            # print(input_code, file=f)
-            # f.write(input_code)
-        # 
-# 
-        # """
-        # Return take output of laait and return output json 
-        # """
-        # system('main_interpreater evaluator notebook > output_.txt')
-        # if command is successful it create a output file in 
-        # output = []
-        # with open('output_.txt', 'r') as f:
-            # output = f.readlines()
-        # if not output:
-            # output = 'No output. It is under developement'
-        # output = ''.join(output)
-        # return log_and_respond(data = output, status = status.HTTP_200_OK,message="Interpreted Successfully")
-    # except Exception as e:
-        # return log_and_respond(
-            # data = None,
-            # message = str(e),
-            # status=  status.HTTP_500_INTERNAL_SERVER_ERROR,
-            # exception=e)
--- laait_backend/shared/shared_utils.py
+++ laait_backend/shared/shared_utils.py
@@ -1,7 +1,13 @@
 from rest_framework.response import Response
+from fnmatch import fnmatch as bash_re
 
 def log_and_respond(data=None, status=None, message=None, exception=None):
     return Response(data={
         "data" : data,
         "message" : message,
     }, status=status, exception=exception)
+
+
+def check_error_message(msg):
+    pattern = "*ERROR*"
+    return bash_re(msg, pattern)
