a2283c2587a07719a0779b01574b5cfd8d34ddf5
Rahul Beniwal <rahul.b@lendenclub.com>
2022-05-21 06:26:30 +0530

    making views cleaner

--- laait_backend/backend/notebook_view_helper.py
+++ laait_backend/backend/notebook_view_helper.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from datetime import datetime
-
+from shared.shared_utils import check_error_message
+from backend.models import Notebook as NB
 from pytz import timezone
 
 def generate_key_from_datetime(datetime):
@@ -27,3 +28,50 @@ def prepare_data_for_history_view(query_set_data):
 
 def get_current_dtm():
     return datetime.now(timezone("Asia/Kolkata"))
+
+
+def check_if_python_code(code):
+    return code.startswith("Python{") or code.startswith("python{") \
+        or code.startswith("python {") or code.startswith("Python {")
+
+
+def execute_python_code(code):
+    if check_if_python_code(code):
+        code = code.split("\n")[1:-1]
+        code = "\n".join(code)
+        output = exec(code)
+        return output
+
+def check_if_python_code_interpreted():
+    with open('output_.txt', 'r') as f:
+        output = f.readlines()
+    if not output:
+        return False 
+
+
+
+def print_output(file_path):
+    response_code = ''
+    output = []
+    with open('output_.txt', 'r') as f:
+        output = f.readlines()
+    if not output:
+        output = 'No output. It is under developement'
+        response_code = 'DEVEL'
+
+    output = ''.join(output)
+    if output:
+        if check_error_message(output):
+            response_code = 'ERROR'
+        else:
+            response_code = 'SUCCESS'
+    return output, response_code
+
+
+def create_notebook_object(input_code, output):
+    return NB.objects.create(
+        input=input_code, 
+        output=output, 
+        created_date=get_current_dtm(), 
+        update_date=get_current_dtm(),
+    )
